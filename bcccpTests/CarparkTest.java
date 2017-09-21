8e946e716691a8beba0d27f4de20c7dd1855e308
Junaid <asadlife89@gmail.com>
2017-09-21 10:08:52 +0530

    Unit Tests

---
+++ bcccpTests/CarparkTest.java
@@ -0,0 +1,29 @@
+package bcccpTests;
+
+import org.junit.Test;
+
+import bcccp.carpark.Carpark;
+import bcccp.tickets.season.ISeasonTicketDAO;
+import bcccp.tickets.season.SeasonTicket;
+import bcccp.tickets.season.SeasonTicketDAO;
+
+public class CarparkTest {
+
+	
+	 
+	 @Test(expected = Exception.class)
+	 public void ownerNameNullOrEmpty() throws Exception 
+	 {
+		  Carpark carpark = new Carpark("",100,null,null);
+		  Carpark carpark2 = new Carpark(null,100,null,null);
+	 }
+	 
+	 @Test(expected = Exception.class)
+	 public void capacityZeroOrLesser() throws Exception 
+	 {
+		  Carpark carpark = new Carpark("my parking",0,null,null);
+		  Carpark carpark2 = new Carpark("my parking",-10,null,null);
+	 }
+	 
+	 
+}
